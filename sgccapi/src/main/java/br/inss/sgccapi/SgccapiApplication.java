9773b92ba6baa2bcd61ad02eb27a6aad5bb3914b
Adrias Souza <adriasbrown@hotmail.com>
2024-09-22 23:34:21 -0500

    feat: Criando ambientes de trabalho com Angular e Bootstrap

---
+++ sgccapi/src/main/java/br/inss/sgccapi/SgccapiApplication.java
@@ -0,0 +1,13 @@
+package br.inss.sgccapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgccapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgccapiApplication.class, args);
+	}
+
+}
