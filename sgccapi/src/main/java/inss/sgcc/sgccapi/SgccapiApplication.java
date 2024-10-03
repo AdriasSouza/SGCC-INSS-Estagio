ec3b2a00726e779fdd7f057141cb068c30dbe5a8
Adrias Souza <adriasbrown@hotmail.com>
2024-10-03 13:44:24 -0500

    feat: agora o bootstrap ta funcionando

--- sgccapi/src/main/java/inss/sgcc/sgccapi/SgccapiApplication.java
+++ sgccapi/src/main/java/inss/sgcc/sgccapi/SgccapiApplication.java
@@ -1,4 +1,4 @@
-package br.inss.sgccapi;
+package inss.sgcc.sgccapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- sgccapi/src/test/java/inss/sgcc/sgccapi/SgccapiApplicationTests.java
+++ sgccapi/src/test/java/inss/sgcc/sgccapi/SgccapiApplicationTests.java
@@ -1,4 +1,4 @@
-package br.inss.sgccapi;
+package inss.sgcc.sgccapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
