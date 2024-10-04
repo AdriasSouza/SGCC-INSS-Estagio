d01da724e30fb356740303c09e7cfc6b35e507b8
Mateus <email@example.com>
2024-10-03 20:18:31 -0500

    feat: subindo outras alterações

--- sgccapp/src/app/app.component.ts
+++ sgccapp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DarkmodeService } from './service/darkmode/darkmode.service';
 
@@ -7,13 +7,44 @@ import { DarkmodeService } from './service/darkmode/darkmode.service';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  darkModeService: DarkmodeService = inject(DarkmodeService);
+  title = 'theme-switcher';
 
-  toggleDarkMode(){
-    this.darkModeService.updateDarkmode();
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.initializeTheme();
   }
-  title = 'sgccapp';
+
+  // Função para inicializar o tema baseado em preferências do usuário
+  initializeTheme() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      this.setTheme(savedTheme);
+    } else {
+      this.setTheme('auto');
+    }
+  }
+
+  // Função para alterar o tema
+  setTheme(theme: string) {
+    const htmlElement = document.documentElement;
+
+    if (theme === 'auto') {
+      // Lógica para alternar entre light/dark baseado nas preferências do sistema
+      const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+      theme = darkModeQuery.matches ? 'dark' : 'light';
+    }
+
+    // Adiciona o tema selecionado ao elemento <html>
+    this.renderer.setAttribute(htmlElement, 'data-bs-theme', theme);
+    localStorage.setItem('theme', theme);
+  }
+
+  // Evento que altera o tema quando o botão é clicado
+  changeTheme(theme: string) {
+    this.setTheme(theme);
+  }
 }
