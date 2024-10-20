ca730811b8f5b865b20e08bf88073078b0b3b8d8
Rainbow-In-The-Dark <brunohf131@gmail.com>
2024-10-20 16:46:29 -0500

    Mudança nas configs do Middleware

--- sgccapi/src/src/settings.py
+++ sgccapi/src/src/settings.py
@@ -44,13 +44,14 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'src.urls'
@@ -131,3 +132,8 @@ AUTH_USER_MODEL = 'usuarios.User'
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 # AUTH_PASSWORD_VALIDATORS
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:4200',
+]
