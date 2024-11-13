59a0bdf5e014f3bde763cd2e092841be45718e37
Rainbow-In-The-Dark <brunohf131@gmail.com>
2024-11-12 22:10:03 -0500

    Adicionei o endpoint para deletar user

--- sgccapp/src/app/service/user.service.ts
+++ sgccapp/src/app/service/user.service.ts
@@ -73,6 +73,7 @@ export class UserService implements IService<User> {
     const url = `${this.apiUrl}user_update_admin/${id}/`;
     return this.http.delete<void>(url, { headers });
   }
+  
   getById(id: number): Observable<User> {
     const headers = this.getAuthHeaders();
     return this.http.get<User>(`${this.apiUrl}user_data_admin/${id}/`, { headers });
